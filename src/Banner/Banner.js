a729e5554a77bddccf2a09f7b03607100f4461a3
Idowu <iarifayan@russelsmith.local>
2019-11-27 14:46:25 +0100

    Add Banner and Navbar fixed position

---
+++ src/Banner/Banner.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Banner = styled.div`
+  height: 200px;
+  background: url("/img/banner.jpg") no-repeat;
+  background-position: center;
+  background-size: cover;
+`;
--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -6,11 +6,16 @@ import { Title } from "../Styles/title";
 const NavbarStyled = styled.div`
   background-color: ${pizzaRed};
   padding: 10px;
+  position: fixed;
+  width: 100%;
+  //   left: 0;
+  //   right: 0;
 `;
 
 const Logo = styled(Title)`
   font-size: 20px;
   color: #fff;
+  text-shadow: 1px 1px 2px rgba(225, 225, 225, 0.6);
 `;
 
 export function Navbar() {
